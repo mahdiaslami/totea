c46b426d4b7a9f74e085702ef67c6b0f02e8ede7
mahdiaslami <mahdyaslami@gmail.com>
2025-06-01 06:26:54 +0330

    feat: add TimelineDate

---
+++ src/components/TimelineDate.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AppDate from '@/app/AppDate';
+
+defineProps<{
+  date: AppDate
+}>()
+
+</script>
+
+<template>
+  <div class="space-y-2">
+    <h2 class="text-slate-400">{{ date.getPersianWeekday() }}</h2>
+    <p class="text-lg font-normal">{{ date.getPersianDate() }} {{ date.getPersianMonthName() }} {{ date.getPersianFullYear() }}</p>
+  </div>
+</template>
