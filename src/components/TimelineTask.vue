5edcae671ececd72f2dc3c5a569fa41add67fd13
mahdiaslami <mahdyaslami@gmail.com>
2025-06-01 06:26:44 +0330

    feat: add TimelineTask

---
+++ src/components/TimelineTask.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type Task from '@/app/Task';
+import IconRoundedRectangle from './icons/IconRoundedRectangle.vue';
+import IconSolidCheckRoundedRectangle from './icons/IconSolidCheckRoundedRectangle.vue';
+
+defineProps<{
+  task: Task
+}>()
+
+</script>
+
+<template>
+  <div class="flex flex-row -mx-2">
+      <div class="px-2">
+        <IconSolidCheckRoundedRectangle v-if="task.completedAt" class="fill-sky-600"/>
+        <IconRoundedRectangle v-else class="stroke-slate-300"/>
+      </div>
+      <div class="px-2 grow">
+        <p>{{ task.text }}</p>
+      </div>
+    </div>
+</template>
