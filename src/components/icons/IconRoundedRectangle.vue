4207ec1ade82c446f342f8a63dc2d7580c5fc660
mahdiaslami <mahdyaslami@gmail.com>
2025-06-01 06:20:51 +0330

    feat: add check icons

---
+++ src/components/icons/IconRoundedRectangle.vue
@@ -0,0 +1,15 @@
+<template>
+  <svg width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="black"
+    stroke-width="1.5"
+    xmlns="http://www.w3.org/2000/svg">
+    <rect x="2.75"
+      y="2.75"
+      width="18.5"
+      height="18.5"
+      rx="7.25" />
+  </svg>
+</template>
