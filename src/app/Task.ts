f1e3e8301416b674f00be31c6c03cb5afcb6ebc9
mahdiaslami <mahdyaslami@gmail.com>
2025-06-04 06:47:31 +0330

    feat: add id to Task and DayRepository

--- src/app/Task.ts
+++ src/app/Task.ts
@@ -1,15 +1,18 @@
 import AppDate from "./AppDate";
 
 export type RawTask = {
+  id: string,
   text: string,
   completedAt: string | null,
 }
 
 export default class Task {
+  id: string | null;
   text: string;
   completedAt: AppDate | null;
 
   constructor(text: string, completedAt: AppDate | null = null) {
+    this.id = null
     this.text = text
     this.completedAt = completedAt
   }
