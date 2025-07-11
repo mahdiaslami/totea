fbe7c8442269b6e71b83d49e069efde089f3fc2e
mahdiaslami <mahdyaslami@gmail.com>
2025-07-11 09:02:06 +0330

    feat: remove DaysSwiper and swiperjs

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => tag.startsWith('swiper-')
-        }
-      }
-    }),
+    vue(),
     // vueDevTools(),
   ],
   resolve: {
