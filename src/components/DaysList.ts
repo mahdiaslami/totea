63f6c2a1c5049bdc652bd54f2ff429c7eba35354
mahdiaslami <mahdyaslami@gmail.com>
2025-07-11 08:40:23 +0330

    feat: add DaysList component

---
+++ src/components/DaysList.ts
@@ -0,0 +1,89 @@
+import AppDate from "@/app/support/AppDate"
+
+export default class DaysList {
+  _items: DateItem[]
+  _maxCount: number
+  _loadCount: number
+
+  constructor(loadCount: number, maxCount: number = loadCount * 3) {
+    this._items = []
+    this._maxCount = maxCount
+    this._loadCount = loadCount
+
+    this.init()
+  }
+
+  get items() {
+    return this._items
+  }
+
+  init() {
+    const date = new AppDate()
+    this._items.push({
+      id: date.getTime(),
+      date,
+      elementId: 'date-today'
+    })
+
+    this.append()
+    this.prepend()
+  }
+
+  prepend() {
+    const lastDate = this._items[0].date
+
+    let newItems = this._items
+
+    if (newItems.length >= this._maxCount) {
+      newItems = newItems.slice(0, newItems.length - this._loadCount)
+    }
+
+    newItems.unshift(
+      ...(this._getDays(lastDate, -1 * this._loadCount).reverse())
+    )
+
+    this._items = newItems
+  }
+
+  append() {
+    const lastDate = this._items[this._items.length - 1].date
+
+    let newItems = this._items;
+
+    if (newItems.length >= this._maxCount) {
+      newItems = newItems.slice(this._loadCount)
+    }
+
+    newItems.push(
+      ...this._getDays(lastDate, this._loadCount)
+    )
+
+    this._items = newItems
+  }
+
+  _getDays(lastDate: AppDate, count: number): DateItem[] {
+    let factor = 1;
+
+    if (count < 0) {
+      factor = -1
+    }
+
+    return Array.from(
+      { length: factor * count },
+      (_, i) => {
+        const date = lastDate.duplicate().addDays(factor * (i + 1))
+
+        return {
+          id: date.getTime(),
+          date,
+        }
+      }
+    )
+  }
+}
+
+export type DateItem = {
+  id: number
+  date: AppDate
+  elementId?: string
+}
---
+++ src/components/DaysList.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { onMounted, reactive } from 'vue';
+import DaysList from './DaysList';
+
+const daysList = new DaysList(10)
+
+const data = reactive({
+  items: daysList.items,
+})
+
+
+function handleScroll(ev: Event) {
+  const {
+    scrollTop,
+    clientHeight,
+    scrollHeight
+  } = ev.target as HTMLElement;
+
+  const position = scrollTop + clientHeight
+
+  if (position > scrollHeight * 2 / 3) {
+    daysList.append()
+    data.items = daysList.items
+  } else if (position < scrollHeight * 1 / 3) {
+    daysList.prepend()
+    data.items = daysList.items
+  }
+}
+
+onMounted(() => {
+  document.getElementById("date-today")?.scrollIntoView()
+})
+</script>
+
+<template>
+  <div @scroll="handleScroll"
+    class="h-full overflow-auto">
+    <div v-for="item in data.items"
+      :key="item.id"
+      v-bind:id="item.elementId"
+      class="min-h-5">
+      <slot :date="item.date"></slot>
+    </div>
+  </div>
+</template>
